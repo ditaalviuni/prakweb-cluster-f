434fafd6663fd3d64caf12898292f35871961116
ditaalviuni <65015006+ditaalviuni@users.noreply.github.com>
2020-11-17 20:00:20 +0700

    update latihan dan tugas minggu 2

---
+++ Modul 5 PHP/MengurutkanData.php
@@ -0,0 +1,10 @@
+<?php
+    $mengurutkan = array("lanirne", "aduh", "qifuat", "toda", "anevi", "samid", "kifuat");
+    sort($mengurutkan);
+    
+    foreach($mengurutkan as $x){
+        echo $x;
+        echo "<br>";
+    }
+    
+?>
