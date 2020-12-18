8fbb4f16392a4c81eb9e89ef89321004d31904fd
ditaalviuni <65015006+ditaalviuni@users.noreply.github.com>
2020-12-18 22:50:59 +0700

    Percobaan

---
+++ Modul 12/tambah.php
@@ -0,0 +1,10 @@
+<?php
+include "koneksi.php";
+
+$nim = $_POST["nim"];
+$nama = $_POST["nama"];
+$prodi = $_POST["prodi"];
+
+$sql = "insert into mahasiswa (nim, nama, prodi) values('$nim','$nama','$prodi')";
+$hasil = mysqli_query($kon, $sql);
+?>
